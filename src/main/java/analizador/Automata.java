03f67cb6de280bf4f74a9f417fccc320a4d66db5
David <dav.llwyr@gmail.com>
2021-11-05 16:52:40 -0600

    Es hora de darle funcionalidad al analizador lexico, por el momento solo esta programada la matriz y el alfabeto, pero aun no estan programados los tipos de tokens que devolvera

--- src/main/java/analizador/Automata.java
+++ src/main/java/analizador/Automata.java
@@ -37,33 +37,33 @@ public class Automata {
 
   
 	/** 
-     * A=0  B=1  C=2  E=3   I=4   X=5  Y=6 Z=7 W=8 SF=9
+     * A=0  B=1  C=2  E=3   I=4   s1=5  s2=6 s3=7 s4=8 s5=9
      * ERROR = -1
      */
 
-    int [][] estados = new int[10][13];
+    int [][] estados = new int[10][11];
     {
-        //  /                          N                    "                       "                      S                      G                     L                      -                    D                          0                        P               espacio             salto de linea
+        //  /                       "                         S                      G                     L                      -                    D                       0                        P               espacio             salto de linea
         //A
-        estados[0][0]= 1;       estados[0][1]= -1;    estados[0][2]= 2 ;   estados[0][3]= -1  ;     estados[0][4]= -1  ;   estados[0][5]= 7;    estados[0][6]= 7;       estados[0][7]= 3;    estados[0][8]= 8 ;    estados[0][9]= 9  ;    estados[0][10]= 9 ;   estados[0][11]= -1;     estados[0][12]= -1;     
+        estados[0][0]= 1;       estados[0][1]= 2;     estados[0][2]=-1 ;       estados[0][3]= 7   ;   estados[0][4]= 7;    estados[0][5]= 3;       estados[0][6]= 8;    estados[0][7]=9  ;    estados[0][8]= 9  ;    estados[0][9]= -1;   estados[0][10]= -1;       
         //B
-        estados[1][0]= 4  ;     estados[1][1]= -1 ;   estados[1][2]= -1 ;  estados[1][3]= -1 ;      estados[1][4]= -1 ;    estados[1][5]= -1;   estados[1][6]=-1  ;     estados[1][7]= -1 ;  estados[1][8]= -1 ;   estados[1][9]= -1 ;    estados[1][10]= -1;   estados[1][11]= -1;     estados[1][12]= -1 ; 
+        estados[1][0]= 4  ;     estados[1][1]= -1 ;   estados[1][2]= -1 ;      estados[1][3]= -1 ;    estados[1][4]= -1;   estados[1][5]=-1  ;     estados[1][6]= -1 ;  estados[1][7]= -1 ;   estados[1][8]= -1 ;    estados[1][9]= -1;   estados[1][10]= -1;     
         //C 
-        estados[2][0]= -1 ;     estados[2][1]= 5  ;   estados[2][2]= -1;   estados[2][3]= -1 ;      estados[2][4]= 5 ;     estados[2][5]= -1 ;  estados[2][6]= 5  ;     estados[2][7]= -1;   estados[2][8]= -1  ;  estados[2][9]= -1 ;    estados[2][10]= -1;   estados[2][11]= 5 ;     estados[2][12]= -1;
+        estados[2][0]= -1 ;     estados[2][1]=-1  ;   estados[2][2]= 5 ;       estados[2][3]=-1 ;     estados[2][4]= 5;    estados[2][5]=-1  ;     estados[2][6]= 5 ;   estados[2][7]=  5  ;  estados[2][8]= -1 ;    estados[2][9]= 5 ;   estados[2][10]= -1;     
         //E 
-        estados[3][0]= -1 ;     estados[3][1]= -1 ;   estados[3][2]= -1 ;  estados[3][3]= -1 ;      estados[3][4]= -1 ;    estados[3][5]= -1 ;  estados[3][6]= -1 ;     estados[3][7]= -1;   estados[3][8]=  8 ;   estados[3][9]= -1 ;    estados[3][10]= -1;   estados[3][11]= -1 ;    estados[3][12]= -1 ;   
+        estados[3][0]= -1 ;     estados[3][1]= -1 ;   estados[3][2]= -1 ;      estados[3][3]= -1 ;    estados[3][4]= -1 ;  estados[3][5]= -1 ;     estados[3][6]= -1;   estados[3][7]= -1 ;   estados[3][8]= -1 ;    estados[3][9]= -1;   estados[3][10]= -1 ;       
         //I
-        estados[4][0]= -1 ;     estados[4][1]= 6 ;    estados[4][2]= -1 ;  estados[4][3]= -1 ;      estados[4][4]= 6 ;     estados[4][5]= 6 ;   estados[4][6]= 6  ;     estados[4][7]= -1;   estados[4][8]= -1 ;   estados[4][9]= -1 ;    estados[4][10]= -1;   estados[4][11]= 6 ;     estados[4][12]= -1;
-        //X 
-        estados[5][0]= -1 ;     estados[5][1]= 5  ;   estados[5][2]= -1 ;  estados[5][3]= 9 ;       estados[5][4]= 5 ;     estados[5][5]= -1 ;  estados[5][6]= 5 ;      estados[5][7]= -1;   estados[5][8]= -1 ;   estados[5][9]=-1 ;     estados[5][10]=-1 ;   estados[5][11]= 5 ;     estados[5][12]=-1 ;
-        //Y 
-        estados[6][0]= -1 ;     estados[6][1]= 6  ;   estados[6][2]= -1 ;  estados[6][3]= -1 ;      estados[6][4]= 6 ;     estados[6][5]= 6 ;   estados[6][6]= 6  ;     estados[6][7]= -1;   estados[6][8]= -1 ;   estados[6][9]= -1 ;    estados[6][10]=-1 ;   estados[6][11]= 6 ;     estados[6][12]= -1 ;
-        //Z
-        estados[7][0]= -1 ;     estados[7][1]= 7  ;   estados[7][2]= -1 ;   estados[7][3]= -1 ;     estados[7][4]= -1 ;    estados[7][5]= 7 ;   estados[7][6]= 7 ;      estados[7][7]= -1  ; estados[7][8]= -1 ;   estados[7][9]= -1 ;    estados[7][10]= -1 ;  estados[7][11]= -1 ;    estados[7][12]= -1 ;
-        //W  
-        estados[8][0]= -1 ;     estados[8][1]= -1 ;   estados[8][2]= -1 ;   estados[8][3]= -1 ;     estados[8][4]= -1 ;    estados[8][5]= -1 ;  estados[8][6]= -1 ;     estados[8][7]= -1 ;  estados[8][8]= 8 ;    estados[8][9]= 8 ;     estados[8][10]= -1 ;  estados[8][11]= -1 ;    estados[8][12]= -1 ; 
-        //SF  
-        estados[9][0]= -1 ;     estados[9][1]= -1 ;   estados[9][2]= -1 ;   estados[9][3]= -1 ;     estados[9][4]= -1 ;    estados[9][5]= -1 ;  estados[9][6]= -1 ;     estados[9][7]= -1 ;  estados[9][8]= -1 ;   estados[9][9]= -1 ;    estados[9][10]= -1 ;  estados[9][11]= -1 ;    estados[9][12]= -1 ; 
+        estados[4][0]= -1 ;     estados[4][1]= 6 ;    estados[4][2]= 6  ;      estados[4][3]=-1 ;     estados[4][4]= 6 ;   estados[4][5]= -1 ;     estados[4][6]=  6;   estados[4][7]= 6  ;   estados[4][8]= -1 ;    estados[4][9]= 6 ;   estados[4][10]= -1 ;     
+        //S1 
+        estados[5][0]= -1 ;     estados[5][1]= -1 ;   estados[5][2]= 5 ;       estados[5][3]=-1 ;     estados[5][4]= 5;    estados[5][5]= -1;      estados[5][6]= 5 ;   estados[5][7]= 5  ;   estados[5][8]=-1 ;     estados[5][9]=5  ;   estados[5][10]= -1 ;     
+        //S2
+        estados[6][0]= -1 ;     estados[6][1]=-1  ;   estados[6][2]= 6 ;       estados[6][3]=-1 ;     estados[6][4]= 6 ;   estados[6][5]= -1 ;     estados[6][6]= 6 ;   estados[6][7]= 6  ;   estados[6][8]= -1 ;    estados[6][9]=6  ;   estados[6][10]= 9 ;     
+        //S3
+        estados[7][0]= -1 ;     estados[7][1]= -1 ;   estados[7][2]= -1 ;      estados[7][3]= 7  ;    estados[7][4]= 7 ;   estados[7][5]= 7 ;      estados[7][6]= 7   ; estados[7][7]=  7 ;   estados[7][8]= -1 ;    estados[7][9]= -1 ;  estados[7][10]= -1 ;    
+        //S4
+        estados[8][0]= -1 ;     estados[8][1]= -1 ;   estados[8][2]= -1 ;      estados[8][3]= -1 ;    estados[8][4]= -1 ;  estados[8][5]= -1 ;     estados[8][6]= 8  ;  estados[8][7]= 8 ;    estados[8][8]= -1;     estados[8][9]= -1 ;  estados[8][10]= -1 ;    
+        //S5  
+        estados[9][0]= -1 ;     estados[9][1]= -1 ;   estados[9][2]= -1 ;      estados[9][3]= -1 ;    estados[9][4]= -1 ;  estados[9][5]= -1 ;     estados[9][6]= -1 ;  estados[9][7]= -1 ;   estados[9][8]= -1 ;    estados[9][9]= -1 ;  estados[9][10]= -1 ;     
     }
 
 
@@ -80,7 +80,7 @@ public class Automata {
     public int getNextEstado(int estadoActual, int tipoCaracter){
         int result = -1; //si fuera error        
 
-        if(tipoCaracter>=0 && tipoCaracter<=5 && estadoActual!=-1){ //solo hay 6 tipos de caracter por lo mismo se coloca el 5 porque inicia en 0
+        if(tipoCaracter>=0 && tipoCaracter<=10 && estadoActual!=-1){ //solo hay 11 tipos de caracter por lo mismo se coloca el 10 porque inicia en 0
             result= estados[estadoActual][tipoCaracter];
         }
 
@@ -118,41 +118,62 @@ public class Automata {
      */
     public int getIntTipoCaracter(char caracter) {
         //Definimos el alfabeto restante
-        char[] puntuacion = {'.',',',';',':'};
-        char[] aritmeticos = {'+', '-', '*','/','%'};
-        char[] agrupacion = {'(', ')', '[', ']', '{', '}'};
+        char [] symbols={':',',',';','/','<','>','=','(',')','[',']','{','}','-','_'};
+        //char [] dig={'1','2','3','4','5','6','7','8','9'};
+
+        
+
+        //Si no lo encuentra devuelve un error identificado como -1
         int result = -1;
 
+        //Dependiendo el orden de las columnas, es el valor que debe de devolver en este caso son de 0-13 
+        //        Σ = /, “, S,	G, L, -, D,	0,	P,	espacio,	Salto de linea  
 
 
-        if(Character.isLetter(caracter)){
-            result = 0;
-        }else if(Character.isDigit(caracter)){
-            result = 1;
-        }else if(caracter == '.' && estadoActual!=0){
-            result = 2;
-        }    
-        else{
-            for(char tmp : puntuacion){
-                if(tmp == caracter){
-                    result = 3;
-                    break;
-                }
-            }
-            for(char tmp : aritmeticos){
-                if(tmp==caracter){
-                    
-                    result = 4;
-                    break;
-                }
-            }
-            for(char tmp : agrupacion){
-                if(tmp==caracter){
-                    result = 5;
-                    break;
-                }
+        if(caracter=='/'){
+            result =0;
+        }
+        
+        else if(caracter=='"'){
+            result =1;
+        }
+
+        for(char c:symbols){
+            if(caracter==c){
+                result =2;
             }
         }
+
+        if(caracter=='_'){
+            result =3;
+        }
+
+        else if(Character.isLetter(caracter)){
+            result =4;
+        }
+
+        else if(caracter=='-'){
+            result =5;
+        }
+
+        else if(Character.isDigit(caracter) && caracter!='0'){
+            result =6;
+        }
+
+        else if(caracter=='0'){
+            result =7;
+        }
+
+        //Aqui iria palabra ---> 8
+
+        else if(Character.isSpaceChar(caracter)){
+            result =9;
+        }
+
+        else if(Character.toString(caracter).equals("\n")){
+            result =10;
+        }
+
         
         return result;
     }
@@ -237,6 +258,23 @@ public class Automata {
 
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Metodo encargado de mostrar en una nueva ventana el movimiento dentro de la matriz del automata
+     * @param mov String con el valor o numero del movimiento del automata
+     */
     private void muestraMovimiento(String mov) {
         JFrame frameMov = new JFrame("Mov. Automata");
         JPanel panelMov = new JPanel();
